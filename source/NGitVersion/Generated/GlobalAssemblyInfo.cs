9c792f850c3fe5e76ba38fd5744d8bbe6f5e0c07
Ugur Coruh <ugur.coruh.tr@gmail.com>
2022-01-07 02:19:03 +0300

    NGitVersion removed reason of the github action failure

--- source/NGitVersion/Generated/GlobalAssemblyInfo.cs
+++ source/NGitVersion/Generated/GlobalAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Runtime.InteropServices;
 [assembly:AssemblyCulture                ("")]
 [assembly:AssemblyConfiguration          ("RELEASE")]
 
-[assembly:AssemblyVersion                ("0.0.0.36")]
-[assembly:AssemblyFileVersion            ("0.0.0.36")]
-[assembly:AssemblyInformationalVersion   ("0.0.0.36, Hash c7c9b86, BuildConfig RELEASE, HasLocalChange True")]
+[assembly:AssemblyVersion                ("0.0.0.37")]
+[assembly:AssemblyFileVersion            ("0.0.0.37")]
+[assembly:AssemblyInformationalVersion   ("0.0.0.37, Hash d432dad, BuildConfig RELEASE, HasLocalChange False")]
--- source/NGitVersion/Generated/GlobalAssemblyInfo.h
+++ source/NGitVersion/Generated/GlobalAssemblyInfo.h
@@ -1,6 +1,6 @@
-#define VERSION_COMA   0,0,0,36
-#define VERSION        "0.0.0.36"
-#define FULL_VERSION   "0.0.0.36, Hash c7c9b86, BuildConfig RELEASE, HasLocalChange True"
+#define VERSION_COMA   0,0,0,37
+#define VERSION        "0.0.0.37"
+#define FULL_VERSION   "0.0.0.37, Hash d432dad, BuildConfig RELEASE, HasLocalChange False"
 #define COMPANY        "ucoruh"
 #define PRODUCT        "Project Cleaner"
 #define COPYRIGHT      "Copyright ©  2021 Uğur CORUH"
